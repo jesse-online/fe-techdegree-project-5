5d8c7f1c62bcf3745e41a0a0ebf953693fd93eeb
jesse-online <clearyjd@gmail.com>
2020-08-01 23:01:14 -0400

    Add gallery and JS functionality

---
+++ js/searchGallery.js
@@ -0,0 +1,21 @@
+/**
+ * Search for an <a>-wrapped image by its title or caption and display only those images in the 
+ * .gallery HTML element
+ */
+const searchImg = () => {
+    const input = document.getElementById('search');
+    const filter = input.value.toLowerCase();
+    let a, item, titleValue, captionValue;
+    a = document.querySelectorAll("a");
+    
+    for (let i = 0; i < a.length; i++) {
+        item = a[i];
+        titleValue = item["title"];
+        captionValue = item.getAttribute("data-caption");
+        if ( titleValue.toLowerCase().includes(filter) || captionValue.toLowerCase().includes(filter) ) {
+            item.style.display = "";
+        } else {
+            item.style.display = "none";
+        }
+    }
+};
